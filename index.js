bde41d4367de3176ac5d94e0c6793884e8921f88
GOJO <rishilord2@gmail.com>
2024-09-08 21:34:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to gojopy", "Playing /help"];
 
 
 let currentIndex = 0;
